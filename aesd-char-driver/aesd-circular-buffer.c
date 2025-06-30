321e06c8e10fc468034ff383274a7b22fac58b27
Ciro Donnarumma <ciro.donnarumma89@gmail.com>
2025-06-30 16:33:45 +0200

    Implementation of circular buffer

--- aesd-char-driver/aesd-circular-buffer.c
+++ aesd-char-driver/aesd-circular-buffer.c
@@ -29,10 +29,28 @@
 struct aesd_buffer_entry *aesd_circular_buffer_find_entry_offset_for_fpos(struct aesd_circular_buffer *buffer,
             size_t char_offset, size_t *entry_offset_byte_rtn )
 {
-    /**
-    * TODO: implement per description
-    */
-    return NULL;
+    struct aesd_buffer_entry *p_entry = NULL;
+
+    if ((buffer->in_offs != buffer->out_offs) || (buffer->full))
+    {
+        uint8_t current_offs = buffer->out_offs;
+        size_t  current_size = 0;
+
+        do {
+            if (char_offset < (current_size + buffer->entry[current_offs].size))
+            {
+                *entry_offset_byte_rtn = char_offset - current_size;
+                p_entry = &(buffer->entry[current_offs]);
+            }
+            else
+            {
+                current_size += buffer->entry[current_offs].size;
+                current_offs = (current_offs + 1) % AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED ;
+            }
+        } while((current_offs != buffer->in_offs) && (p_entry == NULL));
+    }
+
+    return p_entry;
 }
 
 /**
@@ -44,9 +62,18 @@ struct aesd_buffer_entry *aesd_circular_buffer_find_entry_offset_for_fpos(struct
 */
 void aesd_circular_buffer_add_entry(struct aesd_circular_buffer *buffer, const struct aesd_buffer_entry *add_entry)
 {
-    /**
-    * TODO: implement per description
-    */
+    buffer->entry[buffer->in_offs] = *add_entry;
+
+    buffer->in_offs = (buffer->in_offs + 1) % AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED;
+
+    if (buffer->full)
+    {
+        buffer->out_offs = (buffer->out_offs + 1) % AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED;
+    }
+    else
+    {
+        buffer->full = (buffer->in_offs == buffer->out_offs);
+    }
 }
 
 /**
